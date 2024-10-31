584a35339b890ccc71be9284deeae45356fa5977
thaole-cpu <lethithao2k6yl@gmail.com>
2024-10-31 08:19:28 +0700

    Update chia du cho 42.py

--- chia du cho 42.py
+++ chia du cho 42.py
@@ -1,6 +1,10 @@
-a=[]
-while len(a) <10:
-   a = [int(i) for i in input().split()]
-for i in range (len(a)):
-       a[i] %= 42
-print(len(set(a)))
+
+if __name__ == '__main__':
+    l = 10
+    f = [0] * 42
+    while l > 0:
+        a = list(map(int, input().split()))
+        l -= len(a)
+        for i in range(len(a)):
+            f[a[i] % 42] = 1
+    print(len([x for x in f if x == 1]))
